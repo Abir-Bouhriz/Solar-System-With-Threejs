6f57f28f05972451132c7b9739d4a7ba1c0d71d1
Abir-Bouhriz <abirbouhrizdaidj@gmail.com>
2023-06-01 10:46:02 +0100

    Create the remaining planets Obj

--- index.js
+++ index.js
@@ -16,22 +16,16 @@ import {
     Raycaster,
     MathUtils,
     Clock,
-    DirectionalLight,
     SphereGeometry,
-    MeshPhongMaterial,
     TextureLoader,
-    IcosahedronGeometry,
-    MeshLambertMaterial,
     AmbientLight,
     PointLight,
-    Group,
     MeshStandardMaterial,
     CubeTextureLoader,
     Object3D,
     RingGeometry,
     DoubleSide
 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import CameraControls from 'camera-controls';
 
 // 1 Scene
@@ -39,16 +33,13 @@ const scene = new Scene();
 const canvas = document.getElementById('three-canvas');
 
 // 2 the object
-const loader = new TextureLoader();
-/*loader.load('./assets/stars_a.jpg', function (texture) {
-    scene.background = texture;
-});*/
 
 // 2.1 GEOMETRY
 const sphereGeometry = new SphereGeometry(0.5);
 const ringGeometry = new RingGeometry(10, 20, 32);
 
 // 2.2 MATERIALS & TEXTURES
+const loader = new TextureLoader();
 const sunMaterial = new MeshBasicMaterial({
     map: loader.load('./assets/sun.jpeg')
 });
@@ -84,7 +75,7 @@ const neptuneMaterial = new MeshStandardMaterial({
     map: loader.load('./assets/neptune.jpg')
 });
 
-// 2.3 CREATION (Mesh(geometry, Material))
+// 2.3 CREATION Mesh(geometry, Material)
 const sun = new Mesh(sphereGeometry, sunMaterial);
 scene.add(sun);
 
@@ -96,29 +87,37 @@ mercury.scale.set(0.2, 0.2, 0.2);
 mercury.position.x += 1;
 
 const venus = new Mesh(sphereGeometry, venusMaterial);
+const venusObj = new Object3D();
+venusObj.add(venus);
+scene.add(venusObj);
 venus.scale.set(0.3, 0.3, 0.3);
 venus.position.x += 1.5;
-sun.add(venus);
 
 const earth = new Mesh(sphereGeometry, earthMaterial);
+const earthObj = new Object3D();
+earthObj.add(earth);
+scene.add(earthObj);
 earth.scale.set(0.3, 0.3, 0.3);
 earth.position.x += 2;
-sun.add(earth);
 
 const moon = new Mesh(sphereGeometry, moonMaterial);
-moon.scale.set(0.2, 0.2, 0.2);
+moon.scale.set(0.3, 0.3, 0.3);
 moon.position.x += 1;
 earth.add(moon);
 
 const mars = new Mesh(sphereGeometry, marsMaterial);
+const marsObj = new Object3D();
+marsObj.add(mars);
+scene.add(marsObj);
 mars.scale.set(0.25, 0.25, 0.25);
 mars.position.x += 2.5;
-sun.add(mars);
 
 const jupiter = new Mesh(sphereGeometry, jupiterMaterial);
+const jupiterObj = new Object3D();
+jupiterObj.add(jupiter);
+scene.add(jupiterObj);
 jupiter.scale.set(0.5, 0.5, 0.5);
 jupiter.position.x += 3;
-sun.add(jupiter);
 
 const saturn = new Mesh(sphereGeometry, saturnMaterial);
 const saturnObj = new Object3D();
@@ -135,14 +134,18 @@ saturnRing.rotateX(-1.57);
 
 
 const uranus = new Mesh(sphereGeometry, uranusMaterial);
+const uranusObj = new Object3D();
+uranusObj.add(uranus);
+scene.add(uranusObj);
 uranus.scale.set(0.35, 0.35, 0.35);
 uranus.position.x += 4;
-sun.add(uranus);
 
 const neptune = new Mesh(sphereGeometry, neptuneMaterial);
+const neptuneObj = new Object3D();
+neptuneObj.add(neptune);
+scene.add(neptuneObj);
 neptune.scale.set(0.3, 0.3, 0.3);
 neptune.position.x += 4.5;
-sun.add(neptune);
 
 // 3 the camera
 const sizes = {
@@ -162,15 +165,14 @@ renderer.setSize(sizes.clientWidth, sizes.clientHeight, false);
 
 
 // 5 lights
-/*let ambientLight = new AmbientLight(0xffffff, 0.7);
-ambientLight.castShadow = false;
-scene.add(ambientLight);*/
 const pointLight = new PointLight(0xFFFFFF, 2, 300);
 scene.add(pointLight);
 
 const ambientLight = new AmbientLight(0x333333);
 scene.add(ambientLight);
 
+
+// 6 SCENE BACKGROUND
 const cubeTextureLoader = new CubeTextureLoader();
 scene.background = cubeTextureLoader.load([
     './assets/stars.jpg',
@@ -182,7 +184,7 @@ scene.background = cubeTextureLoader.load([
 ]);
 
 
-// 6 responsivity
+// 7 responsivity
 window.addEventListener('resize', () => {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
@@ -190,7 +192,7 @@ window.addEventListener('resize', () => {
 })
 
 
-// 7 Controls
+// 8 Controls
 const subsetOfTHREE = {
     MOUSE,
     Vector2,
@@ -215,34 +217,32 @@ cameraControls.dollyToCursor = true;
 
 
 
-// 8 the animation
+// 9 the animation
 function animate() {
     const delta = clock.getDelta();
     cameraControls.update(delta);
 
-    //sun.rotation.y += 0.004;
-    //earth.rotation.y += 0.004;
-    mercury.rotation.y += 0.004;
-    mercuryObj.rotation.y += 0.004;
-    // venus.rotation.y += 2;
-    // mars.rotation.y += 0.5;
-    // neptune.rotation.y += 1.5;
-    // uranus.rotation.y += 2.5;
+    //sun.rotation.y += 0.0009;
+    earth.rotation.y += 0.008;
+    earthObj.rotation.y += 0.008;
+    mercury.rotation.y += 0.005;
+    mercuryObj.rotation.y += 0.005;
+    venus.rotation.y += 0.002;
+    venusObj.rotation.y += 0.002;
+    mars.rotation.y += 0.005;
+    marsObj.rotation.y += 0.005;
     saturn.rotation.y += 0.038;
     saturnObj.rotation.y += 0.0009;
-    //jupiter.rotation.y += 4;
+    jupiter.rotation.y += 0.0034;
+    jupiterObj.rotation.y += 0.0034;
+    uranus.rotation.y += 0.0025;
+    uranusObj.rotation.y += 0.0025;
+    neptune.rotation.y += 0.0015;
+    neptuneObj.rotation.y += 0.0015;
 
 
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
-    return {
-        render({ time }) {
-            sun.rotation.y = time * 0.05
-
-            mercuryGroup.rotation.y = time * 0.5;
-            mercury.rotation.y = time * 0.20;
-        }
-    }
 }
 
 animate();
